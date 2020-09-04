b529babd7f4873f8f7fe0e5918a05455ded836ca
PaulaFernanandez <pauuagustina@gmail.com>
2020-09-04 00:23:41 -0300

    Correccion de errores

--- FernandezPaula.py
+++ FernandezPaula.py
@@ -1,7 +1,7 @@
 Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 10:41:24) [MSC v.1900 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>>
-Ingreso= (int(input("ELIJA UNA OCPION: INGRESAR CLIENTE= 1 \n, SALIR=0))
+Ingreso=int(input("ELIJA UNA OCPION: INGRESAR CLIENTE= 1 \n SALIR=0))
 while Ingreso ==1:
     ELEGIR=int(input("ELIJA UNA OPCION: HONDA=1 \n YAMAHA=2 \n SUZUKI = 3 \n SALIR = 0"))
     while ELEGIR >=1 and ELEGIR<=4:
@@ -11,9 +11,12 @@ while Ingreso ==1:
         if ELEGIR ==2:
             DESC=(PRECIO*8)/100
         if ELEGIR==3:
-            DESC==(PRECIO*10)/100
+            DESC=(PRECIO*10)/100
         if ELEGIR ==4:
-            DESC==(PRECIO*2)/100
+            DESC=(PRECIO*2)/100
             PRECIOTOTAL=PRECIO-DESC
         print("TOTAL: $",PRECIOTOTAL)
         ELEGIR=int(input("ELIJA UNA OPCION: HONDA=1 \n YAMAHA=2 \n SUZUKI=3 \n SALIR=0))
+    print("ERROR ELIJA OTRA OPCION:")
+    if ELEGIR ==0:
+        Ingreso=int(input(ELIJA UNA OPCION: INGRESAR CLIENTE= 1 \n SALIR =0))
